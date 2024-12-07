ea701618aa82384319f8a7423ffd1cb25cc2f4d9
DAMPFERS <vlad.razyvaev.2805@mail.ru>
2024-12-07 22:49:23 +0700

    update client and ReadMe

--- cli.py
+++ cli.py
@@ -5,11 +5,25 @@ import threading
 SERVER = '127.0.0.1'
 PORT = 1984
 
-t = [1,0,1,0,0,-1,-1,1,1]
-t = [2,0,0,1,0,-1,-1,1,1]
-t = [3,0,0,0,1,-1,1,1,1]
-t = [4,0,0,1,0,-1,-1,-1,-1]
+
 
 client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
-client.sendall(bytes("Алоха", 'UTF-8'))
+client.sendall(bytes("Алоха 0_o", 'UTF-8'))
+
+def task():
+    while True:
+        in_data = client.recv(4096)
+        print("От сервера: ", in_data.decode(encoding="utf-8"))
+        
+def task2():
+    while True:
+        out_data = input()
+        client.sendall(bytes(out_data,"utf-8"))
+        print("Отправлено: " + str(out_data))
+        
+t1 = Thread(target=task2)
+t2 = Thread(target=task)
+
+t1.start()
+t2.start()
--- server_test.py
+++ server_test.py
@@ -1,51 +1,46 @@
-import socket, threading
+import socket, threading 
 
-#192.168.0.1
-#server = socket.socket(socket.AF_INET, socket.SOCK_STERAM)
-#server.bind(('127.0.0.1', 1984)) почему-то на работает 
+HOST_IP = "127.0.0.1"   # IP-адрес хоста (localhost)
+PORT = 1984             # Порт, на котором будет работать сервер
 
-#def start_my_server():
-#    server = socket.create_server(('127.0.0.1', 1984))
-#    server.listen(4)
-#    print("ОН ВОССТАЛ !!!")
-#    client_socket, address = server.accept()
-#    data = client_socket.recv(1024).decode('utf-8')
-#    print(data)
-#    HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
-#    content = 'Accept my boy.......'.encode('utf-8')
-#    client_socket.send(HDRS.encode('utf-8') + content)
- #   print('What(o_0)?')
+class ClientThread(threading.Thread): # Определение класса ClientThread, наследующего от threading.Thread
 
-#start_my_server()
+    def __init__(self, client_Addres, client_socket): 
+        threading.Thread.__init__(self) # Инициализация потока
+        self.csocket = client_socket # Сохранение сокета клиента
+        print("Новое подключение", client_Addres) # Вывод сообщения о новом подключении
 
-LOCALHOST = '127.0.0.1'
-PORT = 1984
-
-server = socket.create_server((LOCALHOST, PORT))
-print("ОН ВОССТАЛ !!!")
+    def run(self): # Метод, который будет выполняться в новом потоке
+        msg = '' # Инициализация пустой строки для сообщения
+        while True:                             # Бесконечный цикл для обработки сообщений от клиента
+            data = self.csocket.recv(4096)      # Получение данных от клиента (чанк до 4096 байт)
+            msg = data.decode()                 # Декодирование полученных данных в строку
+            print(msg)                          # Вывод полученного сообщения
+            if msg == '':                       # Проверка, пустое ли сообщение (клиент отключился)
+                print("Отключение :(")          # Вывод сообщения об отключении
+                break                           # Выход из цикла
+            elif msg == "Какая-то команда от клиента":                              # Проверка, соответствует ли сообщение определенной команде
+                self.csocket.send(bytes("Ответ сервера клиенту", encoding="utf-8")) # Отправка ответа клиенту
 
 
 
-class ClientThread(threading.Thread):
-    def __init__(self,clientAddress,clientsocket):
-        threading.Thread.__init__(self)
-        self.csocket = clientsocket
-        print ("Новое подключение: ", clientAddress)
-    def run(self):
-        msg = ''
-        while True:
-            data = self.csocket.recv(4096)
-            msg = data.decode()
-            print(msg)
-            if msg == '':
-                print("Ушёл")
-                break
+
+def main(): # Основная функция
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)          # Создание сокета сервера
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)        # Настройка сокета для повторного использования адреса
+
+    server.bind((HOST_IP, PORT))                                        # Привязка сокета к указанному IP-адресу и порту
+    print("Сервер восстал!!!")                                          # Вывод сообщения о запуске сервера
+
+    while True:                                                         # Бесконечный цикл для приема новых подключений
+        server.listen(4)                                                # Прослушивание входящих подключений (максимум зависит от параметра, сейчас максимум 4 в очереди)
+        client_sock, client_Addres = server.accept()                    # Принятие нового подключения
+        new_thread = ClientThread(client_Addres, client_sock)           # Создание нового потока для обработки клиента
+        new_thread.start()                                              # Запуск нового потока
 
 
 
-    
-while True:
-    server.listen(2)
-    clientsock, clientAddress = server.accept()
-    newthread = ClientThread(clientAddress,clientsock)
-    newthread.start()
+
+
+if __name__ == "__main__":  # Проверка, запущен ли скрипт напрямую
+    main()                  # Вызов основной функции
